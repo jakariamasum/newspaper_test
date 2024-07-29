bc80a822aa4b8e0e79652c54e6b05837f0921bf9
jakariamasum <jmphy698@gmail.com>
2024-07-29 18:15:15 +0600

    feat:city create and fetched

--- app/admin/city/add/page.tsx
+++ app/admin/city/add/page.tsx
@@ -1,30 +1,54 @@
+"use client";
+import axiosPublic from "@/lib/axiosPublic";
+import { useState } from "react";
+import { toast, Toaster } from "sonner";
+
 const IndexPage: React.FC = () => {
-    return (
-        <>
-            <div className="container my-4">
-                <div className="flex flex-col md:flex-row gap-8">
-                    <div className="w-full md:w-2/3">
-                        <div className="mb-4">
-                            <p>
-                                Title
-                            </p>
-                            <input
-                                type="text"
-                                placeholder="title"
-                                className="p-2 mt-2 w-full outline-none rounded-md"
-                            />
-                        </div>
-                    </div>
-                    <div className="w-full md:w-1/3">
-                        <div className="border-2 border-main border-dashed rounded-md p-2 my-8">
-                            <button type="submit" className="bg-main flex items-center justify-center w-full text-white px-4 py-2 rounded-md">
-                                Publish
-                            </button>
-                        </div>
-                    </div>
-                </div>
+  const [title, setTitle] = useState("");
+  const handlePublish = async () => {
+    console.log(title);
+    try {
+      const response = await axiosPublic.post("/city", { title });
+
+      if (response.status === 200) {
+        toast.success("City created successfully!");
+        setTitle("");
+      }
+    } catch (error) {
+      toast.error("Failed to create city. Please try again.");
+    }
+  };
+  return (
+    <>
+      <div className="container my-4">
+        <div className="flex flex-col md:flex-row gap-8">
+          <div className="w-full md:w-2/3">
+            <div className="mb-4">
+              <p>Title</p>
+              <input
+                type="text"
+                placeholder="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="p-2 mt-2 w-full outline-none rounded-md"
+              />
             </div>
-        </>
-    );
+          </div>
+          <div className="w-full md:w-1/3">
+            <div className="border-2 border-main border-dashed rounded-md p-2 my-8">
+              <button
+                type="submit"
+                onClick={handlePublish}
+                className="bg-main flex items-center justify-center w-full text-white px-4 py-2 rounded-md"
+              >
+                Publish
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Toaster position="top-right" richColors closeButton />
+    </>
+  );
 };
 export default IndexPage;
--- app/admin/city/page.tsx
+++ app/admin/city/page.tsx
@@ -1,48 +1,12 @@
 import Table from "@/components/admin/Table";
-import Link from "next/link";
+import { useAllCities } from "@/lib/useAllCities";
 
-const IndexPage: React.FC = () => {
-    return (
-        <>
-            <Table
-            title="City"
-            link="/admin/city/add"
-            post={
-                [
-                {
-                    img: "/post/1.jpg",
-                    link: "/news/1",
-                    title: "Supply a Four Piece Set of American Solid Color European and American Style Chemical Fiber Bed Sheets",
-                },
-                {
-                    img: "/post/2.jpg",
-                    link: "/news/1",
-                    title: "China Wholesale Cheap Hand Made Brazilian Virgin Remy Long Human Hair Natural Bone Straight 360 Full HD Transparent Swiss Lace Front Wigs for Black Women",
-                },
-                {
-                    img: "/post/3.jpg",
-                    link: "/news/1",
-                    title: "Natural Bone Straight 360 Full HD Transparent Swiss Lace Front Wigs for Black Women",
-                },
-                {
-                    img: "/post/1.jpg",
-                    link: "/news/1",
-                    title: "Supply a Four Piece Set of American Solid Color European and American Style Chemical Fiber Bed Sheets",
-                },
-                {
-                    img: "/post/2.jpg",
-                    link: "/news/1",
-                    title: "China Wholesale Cheap Hand Made Brazilian Virgin Remy Long Human Hair Natural Bone Straight 360 Full HD Transparent Swiss Lace Front Wigs for Black Women",
-                },
-                {
-                    img: "/post/3.jpg",
-                    link: "/news/1",
-                    title: "Natural Bone Straight 360 Full HD Transparent Swiss Lace Front Wigs for Black Women",
-                },
-                ]
-            }
-            />
-        </>
-    );
+const IndexPage: React.FC = async () => {
+  const cities = await useAllCities();
+  return (
+    <>
+      <Table title="City" link="/admin/city" post={cities} />
+    </>
+  );
 };
 export default IndexPage;
