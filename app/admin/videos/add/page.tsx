163b0a4d51dff002c78c0842c5961c07937a3c94
jakariamasum <jmphy698@gmail.com>
2024-08-18 22:30:21 +0600

    feat:videos

--- app/admin/videos/add/page.tsx
+++ app/admin/videos/add/page.tsx
@@ -1,101 +1,172 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Checkbox from "@/components/admin/Checkbox";
 import Content from "@/components/admin/Content";
 import Image from "next/image";
 import Tag from "@/components/admin/Tag";
+import { useAllCategory } from "@/lib/useAllCategory";
+import { useAllSubCategories } from "@/lib/useAllSubCategory";
+import axiosPublic from "@/lib/axiosPublic";
+import { toast } from "sonner";
 
 const extractYouTubeID = (url: string): string | null => {
-    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=|shorts\/)|youtu\.be\/|youtube-nocookie\.com\/embed\/)([a-zA-Z0-9_-]{11})/;
-    const match = url.match(regex);
-    return match ? match[1] : null;
+  const regex =
+    /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=|shorts\/)|youtu\.be\/|youtube-nocookie\.com\/embed\/)([a-zA-Z0-9_-]{11})/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
 };
 
-
 const IndexPage: React.FC = () => {
-    const [title, setTitle] = useState("");
-    const [videoInput, setVideoInput] = useState("");
+  const [title, setTitle] = useState("");
+  const [videoInput, setVideoInput] = useState("");
+  const [tags, setTags] = useState<string[]>(["hello"]);
+  const [description, setDescription] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [subCategories, setSubCategories] = useState([]);
+  const [category, setCategory] = useState<{
+    category: string;
+    subCategory?: string;
+  }>({ category: "" });
 
-    const handleVideoInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const url = e.target.value;
-        const id = extractYouTubeID(url);
-        setVideoInput(id || url);
+  const handleVideoInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const url = e.target.value;
+    const id = extractYouTubeID(url);
+    setVideoInput(id || url);
+  };
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await useAllCategory();
+      setCategories(response);
+    };
+    fetchCategories();
+    const fetchSubCategories = async () => {
+      const response = await useAllSubCategories();
+      setSubCategories(response);
+    };
+    fetchSubCategories();
+  }, []);
+
+  const transformData = (
+    subCategories: { _id: string; title: string; category: string }[],
+    categories: { _id: string; title: string }[]
+  ) => {
+    console.log(subCategories, categories);
+    return categories.map((category) => ({
+      title: category.title,
+      _id: category._id,
+      subCategories: subCategories
+        .filter((subCategory) => subCategory.category === category._id)
+        .map((subCategory) => ({
+          title: subCategory.title,
+          _id: subCategory._id,
+        })),
+    }));
+  };
+
+  const transformeCategorydData = transformData(subCategories, categories);
+
+  const handlePublish = async () => {
+    const formData = {
+      title,
+      video: videoInput,
+      content: description,
+      tags,
+      category,
     };
 
-    return (
-        <>
-            <div className="container my-4">
-                <div className="flex flex-col md:flex-row gap-8">
-                    <div className="w-full md:w-2/3">
-                        <div className="mb-4">
-                            <p>Title</p>
-                            <input
-                                type="text"
-                                placeholder="title"
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
-                                className="p-2 mt-2 w-full outline-none rounded-md"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <p>YouTube Video</p>
-                            <input
-                                type="text"
-                                placeholder="Paste YouTube URL"
-                                value={videoInput}
-                                onChange={handleVideoInputChange}
-                                className="p-2 mt-2 w-full outline-none rounded-md"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <p>Description</p>
-                            <Content />
-                        </div>
+    console.log(formData);
+    try {
+      const response = await axiosPublic.post("/videos", formData);
+      console.log(response.data);
+      if (response.status === 200) {
+        toast.success("Videos published!");
+      } else {
+        toast.error("Failed to publish videos!");
+      }
+    } catch (error) {
+      console.error("Error publishing video:", error);
+      toast.error("Error publishing video:");
+    }
+  };
 
-                        <div className="mb-4">
-                            <p>
-                                Keywords
-                            </p>
-                            <Tag keyword="hello 1, hello 2" />
-                        </div>
-
-                    </div>
-                    <div className="w-full md:w-1/3">
-                        <div className="border-2 border-main border-dashed rounded-md p-2 my-8">
-                            <button type="submit" className="bg-main flex items-center justify-center w-full text-white px-4 py-2 rounded-md">
-                                Publish
-                            </button>
-                        </div>
-
-                        {videoInput && (
-                            <div className="mb-4">
-                                <p>Image Preview</p>
-                                <div className="bg-white p-2 rounded-md mt-2">
-                                    <Image
-                                        src={`https://i.ytimg.com/vi/${videoInput}/mqdefault.jpg`}
-                                        width={400}
-                                        height={400}
-                                        alt="YouTube Video Thumbnail"
-                                        className="w-full rounded-md"
-                                    />
-                                </div>
-                            </div>
-                        )}
-
-                        <Checkbox
-                            title="Category"
-                            items={[
-                                { title: "Category 1", subItems: [{ title: "SubCategory 1.1" }, { title: "SubCategory 1.2" }] },
-                                { title: "Category 2", subItems: [{ title: "SubCategory 2.1", checked: true }] },
-                                { title: "Category 3", checked: true },
-                                { title: "Category 4" }
-                            ]}
-                        />
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="container my-4">
+        <div className="flex flex-col md:flex-row gap-8">
+          <div className="w-full md:w-2/3">
+            <div className="mb-4">
+              <p>Title</p>
+              <input
+                type="text"
+                placeholder="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="p-2 mt-2 w-full outline-none rounded-md"
+              />
             </div>
-        </>
-    );
+            <div className="mb-4">
+              <p>YouTube Video</p>
+              <input
+                type="text"
+                placeholder="Paste YouTube URL"
+                value={videoInput}
+                onChange={handleVideoInputChange}
+                className="p-2 mt-2 w-full outline-none rounded-md"
+              />
+            </div>
+            <div className="mb-4">
+              <p>Description</p>
+              <Content value={description} onChange={setDescription} />
+            </div>
+
+            <div className="mb-4">
+              <p>Keywords</p>
+              <Tag value={tags} onChange={setTags} />
+              <ul>
+                {tags.map((tag, index) => (
+                  <li key={index}>{tag}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3">
+            <div className="border-2 border-main border-dashed rounded-md p-2 my-8">
+              <button
+                type="submit"
+                onClick={handlePublish}
+                className="bg-main flex items-center justify-center w-full text-white px-4 py-2 rounded-md"
+              >
+                Publish
+              </button>
+            </div>
+
+            {videoInput && (
+              <div className="mb-4">
+                <p>Image Preview</p>
+                <div className="bg-white p-2 rounded-md mt-2">
+                  <Image
+                    src={`https://i.ytimg.com/vi/${videoInput}/mqdefault.jpg`}
+                    width={400}
+                    height={400}
+                    alt="YouTube Video Thumbnail"
+                    className="w-full rounded-md"
+                  />
+                </div>
+              </div>
+            )}
+
+            <Checkbox
+              title="Category"
+              items={transformeCategorydData}
+              onChange={setCategory}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default IndexPage;
