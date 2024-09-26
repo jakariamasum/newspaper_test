977481556112f3bf7189d4ca87b3012aaae8eeed
jakariamasum <jmphy698@gmail.com>
2024-09-26 12:19:15 +0600

    feat: story details page

--- app/admin/stories/page.tsx
+++ app/admin/stories/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import Image from "next/image";
-import { FaCalendarAlt } from "react-icons/fa";
-import moment from "moment";
 import { useState, useEffect } from "react";
 import axiosPublic from "@/lib/axiosPublic";
 import Link from "next/link";
@@ -19,14 +17,13 @@ interface Story {
   category: { title: string };
   subCategory?: { title: string };
   banners: Banner[];
-  createdAt: string;
-  updatedAt: string;
 }
 
 const IndexPage: React.FC = () => {
   const [stories, setStories] = useState<Story[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  console.log(stories);
 
   useEffect(() => {
     const fetchStories = async () => {
@@ -42,6 +39,10 @@ const IndexPage: React.FC = () => {
     fetchStories();
   }, []);
 
+  const handleDelete = (id: string) => {
+    console.log("Delete story with id: ", id);
+  };
+
   if (loading) return <Loader />;
   if (error) return <p className="text-center text-red-500">Error: {error}</p>;
 
@@ -55,61 +56,78 @@ const IndexPage: React.FC = () => {
           Add New Story
         </Link>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {stories.map((story) => (
-          <div
-            key={story._id}
-            className="bg-white border border-gray-300 rounded-lg shadow-lg p-4 transition-transform transform hover:scale-105 hover:shadow-xl"
-          >
-            <div className="flex flex-col space-y-4">
-              <div className={`grid grid-cols-${story.banners.length} gap-2`}>
-                {story.banners.map((banner, index) => (
-                  <div key={banner._id} className="relative">
+
+      <div className="overflow-x-auto shadow-lg rounded-lg">
+        <table className="min-w-full bg-white rounded-lg border border-gray-200">
+          <thead>
+            <tr className="bg-gray-100 text-left">
+              <th className="py-4 px-6 text-gray-600 font-semibold text-sm uppercase tracking-wider">
+                Image
+              </th>
+              <th className="py-4 px-6 text-gray-600 font-semibold text-sm uppercase tracking-wider">
+                Title
+              </th>
+              <th className="py-4 px-6 text-gray-600 font-semibold text-sm uppercase tracking-wider">
+                Details
+              </th>
+              <th className="py-4 px-6 text-gray-600 font-semibold text-sm uppercase tracking-wider">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {stories.map((story) => (
+              <tr
+                key={story._id}
+                className="hover:bg-gray-50 transition-colors"
+              >
+                <td className="py-4 px-6">
+                  {story.banners.length > 0 && (
                     <Image
-                      src={banner.img}
-                      alt={banner.title}
-                      width={450}
-                      height={300}
-                      className="rounded-lg object-cover h-48 w-full"
+                      src={story.banners[0].img}
+                      alt={story.banners[0].title}
+                      width={100}
+                      height={80}
+                      className="rounded-lg object-cover w-24 h-16"
                     />
-                    <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-center p-2 rounded-b-lg">
-                      {banner.title}
-                    </div>
-                  </div>
-                ))}
-              </div>
-              <div className="flex flex-col space-y-2">
-                <h2 className="text-xl font-semibold text-gray-800">
-                  {story.title}
-                </h2>
-                <p className="text-gray-600">
-                  <span className="bg-blue-100 text-blue-700 text-xs font-medium px-2.5 py-0.5 rounded-full mr-3">
-                    {story.category.title}
-                  </span>
-                  {story?.subCategory?.title && (
-                    <span className="bg-main text-white text-xs font-medium px-2.5 py-0.5 rounded-full">
-                      {story?.subCategory?.title}
-                    </span>
                   )}
-                </p>
-                <p className="text-sm text-gray-500 flex items-center gap-1">
-                  <FaCalendarAlt />
-                  <span>
-                    : {moment(story.createdAt).format("MMMM Do YYYY")}
-                  </span>
-                </p>
-                <Link
-                  href={`/story/${story._id}`}
-                  className="mt-4 inline-block text-center bg-blue-600 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
-                >
-                  See Details
-                </Link>
-              </div>
-            </div>
-          </div>
-        ))}
+                </td>
+
+                <td className="py-4 px-6 text-gray-800">
+                  <h2 className="font-medium text-lg">{story.title}</h2>
+                </td>
+                <td className="py-4 px-6 text-gray-800">
+                  <Link
+                    href={`/story/${story._id}`}
+                    className="bg-blue-600 text-white py-1 px-3 rounded-lg shadow hover:bg-blue-700 hover:underline"
+                  >
+                    Show Details
+                  </Link>{" "}
+                </td>
+
+                <td className="py-4 px-6">
+                  <div className="flex space-x-2">
+                    <Link
+                      href={`/admin/stories/edit/${story._id}`}
+                      className="bg-green-600 text-white py-1 px-3 rounded-lg shadow hover:bg-green-700"
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      onClick={() => handleDelete(story._id)}
+                      className="bg-red-600 text-white py-1 px-3 rounded-lg shadow hover:bg-red-700"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 };
+
 export default IndexPage;
