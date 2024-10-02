c93ef990ee79146b2c898b7c0f70680512cc7cc7
jakariamasum <jmphy698@gmail.com>
2024-10-02 21:57:48 +0600

    feat: fix header issue

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useRef } from "react";
+import { useRef, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
@@ -25,6 +25,7 @@ type Item = {
   title: string;
   link: string;
   option?: string;
+  //   option?: 'sub' | 'post' | 'tab';
   subItems?: SubItem[];
   post?: Post[];
   limit?: string;
@@ -36,55 +37,51 @@ type MenuProps = {
 };
 
 const Menu: React.FC<MenuProps> = ({ items }) => {
-  const [activeTab, setActiveTab] = useState<number>(0);
+  const [activeTab, setActiveTab] = useState<number>(0); // Track active tab index, default to 0
   const [hoverIndex, setHoverIndex] = useState<number | null>(null);
   const menuRef = useRef<HTMLDivElement>(null);
-
   const handleScroll = (direction: "left" | "right") => {
     if (menuRef.current) {
       const scrollAmount = direction === "left" ? -200 : 200;
       menuRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
     }
   };
-
   return (
-    <div className="relative z-50 block bg-main mb-10 lg:mb-0">
-      <div className="container flex items-center">
+    <div className="z-50 block sticky top-0 bg-main mb-12 lg:mb-0">
+      <div className="container">
         <button
           className="lg:hidden absolute left-0 z-10 p-2 text-white bg-gray-900 bg-opacity-60 hover:bg-opacity-80 rounded-full shadow-md top-[30px]"
           onClick={() => handleScroll("left")}
         >
           <FaArrowLeft className="text-2xl" />
         </button>
-
         <div
+          className="flex space-x-4 overflow-x-auto lg:overflow-visible scrollbar-hide"
           ref={menuRef}
-          className="flex space-x-4 overflow-x-auto scrollbar-hide w-full no-scrollbar relative"
         >
           {items.map((item, index) => (
             <div
               key={index}
-              className={`relative ${item.option === "sub" ? "relative" : ""}`}
+              className={` ${item.option === "sub" ? "relative" : ""}`}
               onMouseEnter={() => setHoverIndex(index)}
               onMouseLeave={() => setHoverIndex(null)}
             >
               <Link
                 href={item.link}
-                className="p-2 text-white block hover:bg-white hover:text-main whitespace-nowrap transition-all duration-300 ease-in-out"
+                className="p-2 text-white block hover:bg-white hover:text-main"
               >
                 {item.title}
               </Link>
-
               {hoverIndex === index &&
                 item.option === "sub" &&
                 item.subItems && (
-                  <div className="absolute left-0 bg-white shadow-lg w-52 transition-all duration-300">
+                  <div className="absolute left-0 bg-white shadow-lg w-52">
                     <div className="flex flex-col">
                       {item.subItems.map((subItem, subIndex) => (
                         <Link
                           key={subIndex}
                           href={subItem.link}
-                          className="p-2 hover:bg-main hover:text-white transition-colors duration-300"
+                          className="p-2 hover:bg-main hover:text-white"
                         >
                           {subItem.title}
                         </Link>
@@ -92,27 +89,26 @@ const Menu: React.FC<MenuProps> = ({ items }) => {
                     </div>
                   </div>
                 )}
-
               {hoverIndex === index && item.option === "post" && item.post && (
-                <div className="absolute w-full left-0 transition-all duration-300">
+                <div className="absolute w-full left-0">
                   <div className="container">
-                    <div className="w-full bg-white grid grid-cols-2 md:grid-cols-5 gap-2 p-2 shadow-md">
+                    <div className="w-full bg-white grid grid-cols-5 gap-2 p-2">
                       {item.post
                         .slice(0, Number(item.limit))
                         .map((postItem, postIndex) => (
                           <Link
                             href={postItem.link}
                             key={postIndex}
-                            className="p-2 border hover:shadow-lg transition-shadow duration-300"
+                            className="p-2 border"
                           >
                             <Image
                               src={postItem.img}
                               width={300}
                               height={300}
                               alt={postItem.title}
-                              className="w-full max-h-32 h-full object-cover rounded-md"
+                              className="w-full max-h-32 h-full object-cover"
                             />
-                            <p className="text-sm mt-1">{postItem.title}</p>
+                            {postItem.title}
                           </Link>
                         ))}
                     </div>
@@ -123,9 +119,9 @@ const Menu: React.FC<MenuProps> = ({ items }) => {
               {hoverIndex === index &&
                 item.option === "tab" &&
                 item.postTabs && (
-                  <div className="absolute w-full left-0 transition-all duration-300">
+                  <div className="absolute w-full left-0">
                     <div className="container">
-                      <div className="flex w-full bg-white shadow-md">
+                      <div className="flex w-full bg-white">
                         <div className="w-1/5 flex flex-col">
                           {item.postTabs.map((tab, tabIndex) => (
                             <Link
@@ -135,8 +131,8 @@ const Menu: React.FC<MenuProps> = ({ items }) => {
                               className={`p-2 block ${
                                 tabIndex === activeTab
                                   ? "bg-main text-white"
-                                  : "hover:bg-main hover:text-white"
-                              } transition-colors duration-300`}
+                                  : ""
+                              }`}
                             >
                               {tab.title}
                             </Link>
@@ -149,23 +145,21 @@ const Menu: React.FC<MenuProps> = ({ items }) => {
                               onMouseEnter={() => setActiveTab(tabIndex)}
                             >
                               {activeTab === tabIndex && (
-                                <div className="w-full bg-white grid grid-cols-2 md:grid-cols-5 gap-2 p-2">
+                                <div className="w-full bg-white grid grid-cols-5 gap-2 p-2">
                                   {tab.post.map((tabPost, tabPostIndex) => (
                                     <Link
                                       href={tabPost.link}
                                       key={tabPostIndex}
-                                      className="p-2 border hover:shadow-lg transition-shadow duration-300"
+                                      className="p-2 border"
                                     >
                                       <Image
                                         src={tabPost.img}
                                         alt={tabPost.title}
                                         width={300}
                                         height={300}
-                                        className="w-full max-h-32 h-full object-cover rounded-md"
+                                        className="w-full max-h-32 h-full object-cover"
                                       />
-                                      <p className="text-sm mt-1">
-                                        {tabPost.title}
-                                      </p>
+                                      {tabPost.title}
                                     </Link>
                                   ))}
                                 </div>
@@ -180,7 +174,6 @@ const Menu: React.FC<MenuProps> = ({ items }) => {
             </div>
           ))}
         </div>
-
         <button
           className="lg:hidden absolute right-0 z-10 p-2 text-white bg-gray-900 bg-opacity-60 hover:bg-opacity-80 rounded-full shadow-md top-[30px]"
           onClick={() => handleScroll("right")}
