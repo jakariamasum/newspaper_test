4241f869d580a2ab49c461b7462cab0edda92a02
jakariamasum <jmphy698@gmail.com>
2024-10-06 02:20:29 +0600

    fix: news/id

--- app/(root)/news/[id]/page.tsx
+++ app/(root)/news/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import News from "@/components/News";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,7 +10,10 @@ import { useParams, useRouter } from "next/navigation";
 import moment from "moment";
 import { postFormat } from "@/app/utils/postFormat";
 import { getRandomPosts } from "@/app/utils/getRandomPosts";
-import { useAuth } from "@/app/context/authContext";
+import { useLang } from "@/app/context/langContext";
+import { toast, Toaster } from "sonner";
+import { FaCompress } from "react-icons/fa";
+import Head from "next/head";
 
 interface IAds {
   id: string;
@@ -28,15 +31,58 @@ type TNews = {
   img: string;
   category: { category: { title: string; _id: string } };
 };
+type TLanguage = {
+  relatedPost: string;
+  popularPost: string;
+  seeAll: string;
+};
 const IndexPage: React.FC = () => {
+  const { lang } = useLang();
+  const router = useRouter();
   const [page] = useState<number>(1);
   const [ads, setAds] = useState<IAds[]>([]);
   const path = useParams();
   const [news, setNews] = useState<TNews>();
   const [allNews, setAllNews] = useState([]);
   const [categories, setCategories] = useState([]);
-  const { user } = useAuth();
-  const router = useRouter();
+  const url = window.location.href;
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const imageContainerRef = useRef<HTMLDivElement | null>(null);
+  const [langDetails, setLangDetails] = useState<TLanguage | null>(null);
+
+  useEffect(() => {
+    const handleFullScreenChange = () => {
+      if (document.fullscreenElement) {
+        setIsFullScreen(true);
+      } else {
+        setIsFullScreen(false);
+      }
+    };
+
+    document.addEventListener("fullscreenchange", handleFullScreenChange);
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullScreenChange);
+    };
+  }, []);
+
+  const handleImageClick1 = () => {
+    if (imageContainerRef.current) {
+      if (!document.fullscreenElement) {
+        imageContainerRef.current.requestFullscreen().catch((err) => {
+          console.error(`Error attempting to enable full-screen mode: ${err}`);
+        });
+      } else {
+        document.exitFullscreen();
+      }
+    }
+  };
+
+  const handleExitFullScreenClick = () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    }
+  };
+
   useEffect(() => {
     const fetchAllNews = async () => {
       const response = await axiosPublic.get(`/news?lang=all`);
@@ -58,8 +104,16 @@ const IndexPage: React.FC = () => {
       setAds(response.data.data);
     };
     fetchAds();
+    const fetchLanguage = async () => {
+      try {
+        const response = await axiosPublic.get(`/language?lang=${lang}`);
+        setLangDetails(response.data.data);
+      } catch (error) {
+        console.error("Error fetching page data:", error);
+      }
+    };
+    fetchLanguage();
   }, []);
-
   const contentParts = news?.content?.split(/<\/p>/) || [];
   const halfwayIndex = Math.floor(contentParts?.length / 2);
   const allPost = postFormat(allNews, categories);
@@ -67,36 +121,51 @@ const IndexPage: React.FC = () => {
     (item) => item.category === news?.category?.category?.title
   );
   const mostPopularPosts = getRandomPosts(relatedPosts, 5);
+  const handleImageClick = (lang: string, news: any) => {
+    const imgSrc = encodeURIComponent(news?.img || "default.jpg");
+    window.location.href = `/${lang}/gallery?image=${imgSrc}`;
+  };
+
+  const handlePrintClick = (id: string) => {
+    router.push(`/print/${id}`);
+  };
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(url);
+      toast.success("Link copied!");
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong!");
+    }
+  };
 
   return (
     <>
+      <Head>
+        <title>{news?.title}</title>
+
+        <meta property="og:title" content={news?.title} />
+        <meta property="og:image" content={news?.img} />
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="article" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={news?.title} />
+        <meta name="twitter:image" content={news?.img} />
+
+        <meta property="og:image" content={news?.img} />
+        <meta name="pinterest-rich-pin" content="true" />
+      </Head>
       {page === 1 && (
         <div className="container my-2">
-          <button
-            onClick={() => router.back()}
-            className="flex mb-2 items-center space-x-2 bg-blue-600 text-white font-semibold px-4 py-2 rounded-md shadow hover:bg-gray-700 transition duration-200"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 12H3m0 0l7 7m-7-7l7-7"
-              />
-            </svg>
-            <span>Back</span>
-          </button>
           <div className="w-full flex md:flex-row flex-col gap-4">
             <div className="w-fill md:w-3/4 block space-y-2">
               <div className="bg-white p-2  ">
                 <Link
-                  href="/"
+                  href={
+                    `/${lang}/categories/` +
+                      (news?.category?.category?._id as string) || "/"
+                  }
                   className="bg-main text-white px-2 py-0 rounded-sm mb-2 leading-none"
                 >
                   {news?.category?.category?.title || ""}
@@ -135,10 +204,12 @@ const IndexPage: React.FC = () => {
                       >
                         <path d="M352 320c-22.608 0-43.387 7.819-59.79 20.895l-102.486-64.054a96.551 96.551 0 0 0 0-41.683l102.486-64.054C308.613 184.181 329.392 192 352 192c53.019 0 96-42.981 96-96S405.019 0 352 0s-96 42.981-96 96c0 7.158.79 14.13 2.276 20.841L155.79 180.895C139.387 167.819 118.608 160 96 160c-53.019 0-96 42.981-96 96s42.981 96 96 96c22.608 0 43.387-7.819 59.79-20.895l102.486 64.054A96.301 96.301 0 0 0 256 416c0 53.019 42.981 96 96 96s96-42.981 96-96-42.981-96-96-96z"></path>
                       </svg>
-                      <p>Share</p>
+                      <p className="cursor-pointer" onClick={handleCopy}>
+                        Copy
+                      </p>
                     </div>
                     <Link
-                      href="/"
+                      href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}
                       className="bg-[#516eab] p-2 rounded-sm"
                       target="_blank"
                     >
@@ -155,7 +226,7 @@ const IndexPage: React.FC = () => {
                       </svg>
                     </Link>
                     <Link
-                      href="/"
+                      href={`https://twitter.com/intent/tweet?text=${url}`}
                       className="bg-[#29c5f6] p-2 rounded-sm"
                       target="_blank"
                     >
@@ -172,7 +243,7 @@ const IndexPage: React.FC = () => {
                       </svg>
                     </Link>
                     <Link
-                      href="/"
+                      href={`https://pinterest.com/pin/create/button/?url=${url}`}
                       className="bg-[#ca212a] p-2 rounded-sm"
                       target="_blank"
                     >
@@ -189,7 +260,7 @@ const IndexPage: React.FC = () => {
                       </svg>
                     </Link>
                     <Link
-                      href="/"
+                      href={`https://wa.me/?text=${url}`}
                       className="bg-[#7bbf6a] p-2 rounded-sm"
                       target="_blank"
                     >
@@ -205,10 +276,9 @@ const IndexPage: React.FC = () => {
                         <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path>
                       </svg>
                     </Link>
-                    <Link
-                      href={`/print/${news?._id}`}
-                      className="bg-[#7bbf6a] p-2 rounded-sm"
-                      target="_blank"
+                    <span
+                      onClick={() => handlePrintClick(news?._id as string)}
+                      className="bg-[#7bbf6a] p-2 rounded-sm cursor-pointer"
                     >
                       <svg
                         stroke="currentColor"
@@ -244,20 +314,32 @@ const IndexPage: React.FC = () => {
                         ></path>
                         <circle cx="392" cy="184" r="24"></circle>
                       </svg>
-                    </Link>
+                    </span>
                   </div>
                 </div>
               </div>
 
               <div className="bg-white p-2">
                 <AdDisplay ads={ads} adId="detailsImagesTop" />
-                <Image
-                  src={news?.img || "/default.jpg"}
-                  width={696}
-                  height={464}
-                  alt={news?.title || "post"}
-                  className="w-full h-auto"
-                />
+                <div ref={imageContainerRef}>
+                  <Image
+                    src={news?.img || "/default.jpg"}
+                    alt={news?.title || "post"}
+                    layout="responsive"
+                    width={696}
+                    height={464}
+                    className="cursor-pointer object-contain"
+                    onClick={handleImageClick1}
+                  />
+                  {isFullScreen && (
+                    <div
+                      className="absolute top-4 right-4 text-white text-4xl cursor-pointer"
+                      onClick={handleExitFullScreenClick}
+                    >
+                      <FaCompress size={24} fill="blue" />
+                    </div>
+                  )}
+                </div>
               </div>
               <AdDisplay ads={ads} adId="detailsImagesBottom" />
 
@@ -271,13 +353,44 @@ const IndexPage: React.FC = () => {
                 />
 
                 <AdDisplay ads={ads} adId="detailsDescriptionCentre" />
-                <Image
-                  src={news?.img || "/default.jpg"}
-                  width={696}
-                  height={464}
-                  alt={news?.title || "post"}
-                  className="w-min mx-auto h-auto"
-                />
+                <div
+                  ref={imageContainerRef}
+                  className={`relative ${
+                    isFullScreen
+                      ? "fixed inset-0 flex justify-center items-center bg-black z-50"
+                      : "w-full h-auto"
+                  }`}
+                >
+                  {isFullScreen ? (
+                    <Image
+                      src={news?.img || "/default.jpg"}
+                      alt={news?.title || "post"}
+                      layout="responsive"
+                      width={1920}
+                      height={1080}
+                      className="cursor-pointer object-contain"
+                      onClick={handleExitFullScreenClick}
+                    />
+                  ) : (
+                    <Image
+                      src={news?.img || "/default.jpg"}
+                      width={696}
+                      height={464}
+                      alt={news?.title || "post"}
+                      className="w-min mx-auto h-auto cursor-pointer"
+                      onClick={handleImageClick1}
+                    />
+                  )}
+
+                  {isFullScreen && (
+                    <div
+                      className="absolute top-4 right-4 text-white text-4xl cursor-pointer"
+                      onClick={handleExitFullScreenClick}
+                    >
+                      <FaCompress size={24} fill="blue" />
+                    </div>
+                  )}
+                </div>
                 <div
                   className="mb-2"
                   dangerouslySetInnerHTML={{
@@ -292,12 +405,16 @@ const IndexPage: React.FC = () => {
               <AdDisplay ads={ads} adId="detailsRelatedPostTop" />
 
               <News
-                title="RELATED ARTICLES"
-                link="/"
+                title={langDetails?.relatedPost || "RELATED ARTICLES"}
+                link={
+                  `/${lang}/categories/` +
+                    (news?.category?.category?._id as string) || "/"
+                }
                 limit={5}
                 box={14}
                 style={2}
                 item={relatedPosts}
+                seeAll={langDetails?.seeAll}
               />
               <AdDisplay ads={ads} adId="detailsRelatedPostBottom" />
             </div>
@@ -312,12 +429,16 @@ const IndexPage: React.FC = () => {
                   />
                 </Link>
                 <News
-                  title="MOST POPULAR"
-                  link="/"
+                  title={langDetails?.popularPost || "MOST POPULAR"}
+                  link={
+                    `/${lang}/categories/` +
+                      (news?.category?.category?._id as string) || "/"
+                  }
                   limit={5}
                   box={2}
                   style={2}
                   item={mostPopularPosts}
+                  seeAll={langDetails?.seeAll}
                 />
                 <Link href="/" className="mb-2 block bg-white p-2">
                   <Image
@@ -334,26 +455,6 @@ const IndexPage: React.FC = () => {
       )}
       {page === 2 && (
         <div className="container my-2">
-          <button
-            onClick={() => router.back()}
-            className="flex mb-2 items-center space-x-2 bg-blue-600 text-white font-semibold px-4 py-2 rounded-md shadow hover:bg-gray-700 transition duration-200"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 12H3m0 0l7 7m-7-7l7-7"
-              />
-            </svg>
-            <span>Back</span>
-          </button>
           <div className="w-full flex md:flex-row flex-col gap-4">
             <div className="w-fill md:w-3/4 block space-y-2">
               <div className="bg-white p-2">
@@ -362,7 +463,8 @@ const IndexPage: React.FC = () => {
                   width={696}
                   height={464}
                   alt={news?.title || `post`}
-                  className="w-full h-auto"
+                  className="w-full h-auto cursor-pointer"
+                  onClick={() => handleImageClick(lang, news)}
                 />
               </div>
 
@@ -378,7 +480,10 @@ const IndexPage: React.FC = () => {
 
               <div className="bg-white p-2">
                 <Link
-                  href="/"
+                  href={
+                    ((`/${lang}/categories/` +
+                      news?.category?.category?._id) as string) || "/"
+                  }
                   className="bg-main text-white px-2 py-0 rounded-sm mb-2 leading-none"
                 >
                   {news?.category?.category?.title || "Category"}
@@ -393,7 +498,8 @@ const IndexPage: React.FC = () => {
                       width={20}
                       height={20}
                       alt={news?.author?.title || ""}
-                      className="rounded-full"
+                      className="rounded-full cursor-pointer"
+                      onClick={() => handleImageClick(lang, news)}
                     />
                     <span>By</span>
                     <strong>{news?.title || ""}</strong>
@@ -415,7 +521,9 @@ const IndexPage: React.FC = () => {
                       >
                         <path d="M352 320c-22.608 0-43.387 7.819-59.79 20.895l-102.486-64.054a96.551 96.551 0 0 0 0-41.683l102.486-64.054C308.613 184.181 329.392 192 352 192c53.019 0 96-42.981 96-96S405.019 0 352 0s-96 42.981-96 96c0 7.158.79 14.13 2.276 20.841L155.79 180.895C139.387 167.819 118.608 160 96 160c-53.019 0-96 42.981-96 96s42.981 96 96 96c22.608 0 43.387-7.819 59.79-20.895l102.486 64.054A96.301 96.301 0 0 0 256 416c0 53.019 42.981 96 96 96s96-42.981 96-96-42.981-96-96-96z"></path>
                       </svg>
-                      <p>Share</p>
+                      <p className="cursor-pointer" onClick={handleCopy}>
+                        Copy
+                      </p>{" "}
                     </div>
                     <Link
                       href="/"
@@ -572,12 +680,16 @@ const IndexPage: React.FC = () => {
               </Link>
 
               <News
-                title="RELATED ARTICLES"
-                link="/"
+                title={langDetails?.relatedPost || "RELATED ARTICLES"}
+                link={
+                  ((`/${lang}/categories/` +
+                    news?.category?.category?._id) as string) || "/"
+                }
                 limit={5}
                 box={14}
                 style={2}
                 item={relatedPosts}
+                seeAll={langDetails?.seeAll}
               />
 
               <Link href="/" className="mb-2 block bg-white p-2">
@@ -601,12 +713,16 @@ const IndexPage: React.FC = () => {
                   />
                 </Link>
                 <News
-                  title="MOST POPULAR"
-                  link="/"
+                  title={langDetails?.popularPost || "MOST POPULAR"}
+                  link={
+                    ((`/${lang}/categories/` +
+                      news?.category?.category?._id) as string) || "/"
+                  }
                   limit={5}
                   box={2}
                   style={2}
                   item={mostPopularPosts}
+                  seeAll={langDetails?.seeAll}
                 />
                 <Link href="/" className="mb-2 block bg-white p-2">
                   <Image
@@ -621,6 +737,7 @@ const IndexPage: React.FC = () => {
           </div>
         </div>
       )}
+      <Toaster richColors position="top-right" />
     </>
   );
 };
