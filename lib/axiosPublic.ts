1ddffff72edb1899ef3a1786796bf5ce974e7a86
jakariamasum <jmphy698@gmail.com>
2024-10-06 12:59:07 +0600

    feat: dynamic sitemap

--- lib/axiosPublic.ts
+++ lib/axiosPublic.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL ||
-    "https://newspaper-backend-eta.vercel.app/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND,
 });
 
 export default axiosPublic;
