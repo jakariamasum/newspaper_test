a172998a534dec2853bc07fe4fc83bb0f78cc8be
jakariamasum <jmphy698@gmail.com>
2024-10-26 20:56:12 +0600

    fix

--- app/(root)/[lang]/page.tsx
+++ app/(root)/[lang]/page.tsx
@@ -53,9 +53,7 @@ const IndexPage: React.FC = () => {
       try {
         setLoading(true);
 
-        const response = await axiosPublic.get(
-          `/categories?lang=${pathname.lang}`
-        );
+        const response = await axiosPublic.get(`/categories?lang=all`);
         setCategory(response.data.data);
       } catch (error) {
         console.error("Error fetching page data:", error);
