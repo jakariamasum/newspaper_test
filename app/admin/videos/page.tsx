4ee5378f21bb01ed8d545791e35bd23e80184a1e
jakariamasum <jmphy698@gmail.com>
2024-10-28 12:13:46 +0600

    fix

--- app/admin/videos/page.tsx
+++ app/admin/videos/page.tsx
@@ -83,7 +83,7 @@ const IndexPage: React.FC = () => {
                   scope="col"
                   className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  Story
+                  Video
                 </th>
                 <th
                   scope="col"
